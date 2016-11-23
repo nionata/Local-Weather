d383176df8b07fc0098e7a803f60e631fa58bcb3
Nicholas Ionata <n.ionata129@gmail.com>
2016-11-23 18:47:00 -0500

    Basic weather API call

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
+    getWeather();
+});
+
+function getWeather() {
     var lat = 0;
-    var lon = 0;
+    var long = 0;
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            console.log(lat + lon);
-        }
-    });
-
-    /*$.getJSON("api.openweathermap.org/data/2.5/weather?lat={" + lat + "}&lon={" + lon + "}", function(json) {
-       console.log("Congrats you made it this far");
-    });*/
+            long = position.coords.longitude;
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
+                $("#name").text(json.weather[0].description);
+            });
+        });
+    }
 }
