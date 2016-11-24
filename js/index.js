f1981cbe803cd5e0009245c548b90362a8b5e5e9
Nicholas Ionata <n.ionata129@gmail.com>
2016-11-23 20:46:01 -0500

    Cleaned up API call. Changed to storing metric and imperial rather than making new calls each time

--- js/index.js
+++ js/index.js
@@ -1,41 +1,44 @@
+var lat;
+var long;
+var imperial;
+var metric;
+
 $(document).ready(function() {
-    getWeather();
-
-});
-
-function getWeather() {
-    var lat = 0;
-    var long = 0;
-
-    if (navigator.geolocation) {
+    if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             lat = position.coords.latitude;
             long = position.coords.longitude;
-            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=imperial&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
-                $("#name").text(json.weather[0].description);
-                $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png")
-                $("#location").text(json.sys.country);
-                $("#place").text(json.name);
-                $("#temp").text(json.main.temp);
-            });
+            getWeather();
         });
     }
 
     $(".toggle").on("click", function() {
-        var unit = "imperial";
-
+        console.log("Skipper");
         if($(".toggle").attr("id") == "imperial") {
-            unit = "metric";
+            $("#temp").text(metric);
             $(".toggle").attr("id", "metric");
             $(".toggle").text("°C");
         } else if($(".toggle").attr("id") == "metric") {
-            unit = "imperial";
+            $("#temp").text(imperial);
             $(".toggle").attr("id", "imperial");
             $(".toggle").text("°F");
         }
-
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=" + unit + "&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
-            $("#temp").text(json.main.temp);
-        });
     });
-}
+});
+
+function getWeather() {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=imperial&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
+        imperial = json.main.temp;
+        $("#name").text(json.weather[0].description);
+        $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png")
+        $("#location").text(json.sys.country);
+        $("#place").text(json.name);
+        $("#temp").text(imperial);
+    });
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=metric&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
+        metric = json.main.temp;
+    });
+};
+
+
