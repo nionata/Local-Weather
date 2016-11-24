907ee60c4b2638cbb5a4f2424c16fcf42d7131ab
Nicholas Ionata <n.ionata129@gmail.com>
2016-11-23 19:34:07 -0500

    Added temperature unit toggle button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     getWeather();
+
 });
 
 function getWeather() {
@@ -10,12 +11,31 @@ function getWeather() {
         navigator.geolocation.getCurrentPosition(function(position) {
             lat = position.coords.latitude;
             long = position.coords.longitude;
-            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=imperial&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
                 $("#name").text(json.weather[0].description);
                 $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png")
                 $("#location").text(json.sys.country);
                 $("#place").text(json.name);
+                $("#temp").text(json.main.temp);
             });
         });
     }
+
+    $(".toggle").on("click", function() {
+        var unit = "imperial";
+
+        if($(".toggle").attr("id") == "imperial") {
+            unit = "metric";
+            $(".toggle").attr("id", "metric");
+            $(".toggle").text("°C");
+        } else if($(".toggle").attr("id") == "metric") {
+            unit = "imperial";
+            $(".toggle").attr("id", "imperial");
+            $(".toggle").text("°F");
+        }
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=" + unit + "&APPID=4d56e3677c38512b8f95d040c8998b4d", function(json) {
+            $("#temp").text(json.main.temp);
+        });
+    });
 }
